bde5e0dff75bd7c0c7c96543eddaea4c37e9c14f
ryuilhwan <ih.ryu.dev@gmail.com>
2020-02-12 14:08:02 +0900

    .env 설명

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { MongoClient } from 'mongodb'
 import { strict as assert } from 'assert'
-
 (async () => {
   const url = `mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DBNAME}`
   const client = new MongoClient(url, {
@@ -19,3 +18,4 @@ import { strict as assert } from 'assert'
     client.close()
   }
 })()
+console.log(process.env.DB_HOSTNAME)
