fc6a9c972d6c015b9433187b7a0164c05abd0696
ryuilhwan <ih.ryu.dev@gmail.com>
2020-02-12 14:13:53 +0900

    express mongodb 를 이용한 초간단 rest-api제작 - 몽고 설치하고 db랑 collection만들고 seed데이터 넣는 건 각자 꼭 해보세요. 지금은 작동하지 않을 겁니다.작동하게 만들어주세요

--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
 import Express from 'express'
-
+import { MongoClient } from 'mongodb'
 const app = new Express()
 
-app.get('/hello', (req, res) => {
-  res.write('<html><head><title>hello</title></head><body><p>hello world</p></body></html>')
+app.get('/hello', async (req, res) => {
+  const url = `mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DBNAME}`
+  const client = new MongoClient(url, {
+    useNewUrlParser: true
+  })
+  try {
+    await client.connect()
+    const db = client.db(process.env.DBNAME)
+    const dbres = await db.collection('coins').findOne()
+    res.json(dbres)
+  } catch (e) {
+    console.error(e.stack)
+  } finally {
+    client.close()
+  }
+  // res.json({a:1})
+  // res.write('<html><head><title>hello</title></head><body><p>hello world</p></body></html>')
 })
 
 app.listen(3000)
