a35d64f774290f9384605dbf5f10a11f388a299d
ryuilhwan <ih.ryu.dev@gmail.com>
2020-03-14 20:49:58 +0900

    add api

--- api/test/Machine.test.js
+++ api/test/Machine.test.js
@@ -1,13 +1,10 @@
 import { strict as assert } from 'assert'
 import { get_token } from '../src/Machine'
 
-const CLIENT_ID = 'cvpzAi37wmF3KYfyxsONtxvLCcDehjZpPgl3XVlfanBQU2io4CaSFFdfbrMUS'
-const CLIENT_SCRET = '40uSaqULTh8aKhFHjvljrOkheEPMVvgmXGU5QoVuc8d57nOxRuFZCEGj4aWUH'
-
 describe('Machine', function () {
   it('Machine', async function () {
     this.timeout(3000)
-    const res = await get_token(CLIENT_ID, CLIENT_SCRET)
+    const res = await get_token(process.env.CLIENT_ID, process.env.CLIENT_SCRET)
     assert.deepEqual(Object.keys(res), ['access_token', 'expires_in', 'scope', 'refresh_token', 'token_type'])
   })
 })
